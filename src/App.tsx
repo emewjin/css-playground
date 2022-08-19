4b35fc1b915ba39fd58f8ee9ffc2b33aaeb728fe
emewjin <emewjin@gmail.com>
2022-08-19 22:37:43 +0900

    init setting

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { container } from './style.css';
+
+function App() {
+  return (
+    <div className={container}>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
