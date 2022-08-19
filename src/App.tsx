3f0d7fc9caafc285605b36a03993481b543c5549
emewjin <emewjin@gmail.com>
2022-08-19 22:54:13 +0900

    react router dom 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { Link } from 'react-router-dom';
 import { container } from './style.css';
 
 function App() {
   return (
     <div className={container}>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <h1>Home</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="badge">Badge</Link>
+        </nav>
+      </div>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import Badge from './Badge';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="badge" element={<Badge />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
